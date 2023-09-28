edf0d62ccddf6d0618f948b787b968c6f072702d
momo0327 <mohamedsafe0427@gmail.com>
2023-09-28 14:30:10 +0200

    first commit

---
+++ ordbok/src/App.jsx
@@ -0,0 +1,106 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [word, setWord] = useState()
+  const [info, setInfo] = useState()
+  const [error, setError] = useState()
+  
+ // hämtar data från api
+
+  async function postWord() {
+    setInfo('')
+    if(!word){
+      setError("Skriv ett ord för att få ett svar")
+    }else{
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+   console.log(data.message);
+    
+    if (data.message) {
+      setError(data.message)
+    }else{  
+      setError("")   
+    setInfo(data[0]);
+    console.log(data[0]);
+ 
+  }}}
+  console.log(info);
+
+
+// function för ljud
+function audio(audioUrl){
+  const audio = new Audio(audioUrl)
+  audio.play()
+}
+
+
+// lista ist för div
+
+  return (
+    <div>
+      <h1>Ord Bok</h1>
+      <br />
+      <input type="text" placeholder='Skriv ett ord' value={word} onChange={(event)=>{
+       setWord(event.target.value)}}/>
+      <br />
+      <button onClick={postWord}>Sök ord</button>
+      <br />
+     <p>{error}</p>
+     <h1>{info?.word}</h1>
+{info && <details>
+<summary>Phonetics</summary>
+
+     {info?.phonetics.map((phonetic)=>(
+      <div>
+        <div> {phonetic?.text} </div> 
+         {phonetic.audio &&  <button onClick={()=> {audio(phonetic?.audio)}}>Play Audio</button > 
+ }
+
+
+        
+      </div>
+      ))}
+      <br />
+      </details>}
+      
+    { info && <details> <summary>meanings</summary>
+     {info?.meanings.map((meanings)=> (
+       
+          <div>
+            <p >{meanings?.antonyms.join(", ") }</p>
+            <p >{meanings?.partOfSpeech }</p>
+          </div>
+
+     ))}
+     </details>   }  
+
+      {info && <details> <summary> Synonyms</summary>
+     {info?.meanings.map((meanings)=> (
+       
+       <div>
+         <p >{meanings?.synonyms.join(", ") }</p> 
+       </div>
+
+  ))}
+  </details>}
+
+  {info && <details> <summary> definitions</summary>
+     {info?.meanings.map((meanings)=> (
+       
+       <div>
+         <p >{meanings?.definitions.map((defintions)=> (
+              defintions.definition
+         )) }</p>
+       </div>
+
+  ))}
+  </details>}
+    </div>
+  )
+}
+
+export default App
---
+++ ordbok/src/App.test.jsx
@@ -0,0 +1,53 @@
+import { render, screen } from '@testing-library/react';
+import{it,describe, expect} from "vitest"
+import App from './App';
+import userEvent from "@testing-library/user-event"
+
+describe('App', () => {
+  it('shows header', () => {
+    render(<App  />);
+    expect(screen.getByText("Ord Bok"))
+
+    // check if App components renders headline
+  });
+  it("user can search for a word", async () => {
+    render(<App/>)
+    const user = userEvent.setup()
+
+    const input = screen.getByRole("textbox")
+    await user.type(input, "cat")
+    await user.click(screen.getByRole("button"))
+
+    await user.click(await screen.findByText("Phonetics"))
+    expect(screen.getByText("/kæt/")).toBeInTheDocument()
+  })
+});
+// tester för andra kategorier
+// testa för att texten dyker upp om du skriver fel ord
+// testa för att texten dyker upp när du inte skriver något ord
+describe('data', () => {
+  it('returns data from api', async() => {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/strong`
+    const response = await fetch(url)
+    const data = await response.json()
+    expect(data[0].word).toBe('strong');
+
+
+    // check if App components renders headline
+  });
+});
+
+
+
+describe('input', () => {
+  it('returns error message on empty field',async () => {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/strongw`
+    const response = await fetch(url)
+    const data = await response.json()
+    expect(data.message).toBe("Sorry pal, we couldn't find definitions for the word you were looking for.")
+
+
+    // check if App components renders headline
+  });
+});
+
